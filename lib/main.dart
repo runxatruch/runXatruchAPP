65be13905e764cb64689473525704ef71cc7d78f
Angel Chavez <agchavez@unah.hn>
2021-02-04 22:36:06 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:runxatruch_app/pages/home_pages.dart';
+import 'package:runxatruch_app/pages/login_pages.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+      ),
+      initialRoute: 'login',
+      routes: {
+        'login': (BuildContext context) => LoginPages(),
+        'home': (BuildContext context) => HomePages()
+      },
+    );
+  }
+}
---
+++ lib/pages/home_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio'),
+      ),
+      body: Center(
+        child: Text('Pagina principal'),
+      ),
+    );
+  }
+}
