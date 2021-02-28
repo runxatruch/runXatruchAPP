b59792714ebab48f22c170809dc15cc7499f82a5
Daniela Zavala <danny15zavalalic@gmail.com>
2021-02-28 01:15:41 -0600

    first commit

--- lib/pages/account_pages.dart
+++ lib/pages/account_pages.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:runxatruch_app/models/user_models.dart';
-import 'package:runxatruch_app/prefUser/preferent_user.dart';
+//import 'package:runxatruch_app/prefUser/preferent_user.dart';
 import 'package:runxatruch_app/provider/auth_provider.dart';
 import 'package:runxatruch_app/utils/menu_alert.dart';
 import 'package:runxatruch_app/utils/util.dart' as utils;
