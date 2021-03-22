42909b1e10310d0fa51859560e9009253cad60d5
Daniela Zavala <danny15zavalalic@gmail.com>
2021-03-22 01:50:48 -0600

    Function getEventsDates

--- lib/provider/events_provider.dart
+++ lib/provider/events_provider.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 import 'package:runxatruch_app/models/events_model.dart';
 import 'package:runxatruch_app/prefUser/preferent_user.dart';
 
@@ -12,13 +13,40 @@ class EventProvider {
   Future<List<EventModel>> getEvents() async {
     Query firestoreInstance = FirebaseFirestore.instance.collection("event");
     final List<EventModel> events = new List();
-    await firestoreInstance.get().then((value) {
+
+    await firestoreInstance
+        .where("startTime", isLessThanOrEqualTo: monthNext())
+        .where("startTime", isGreaterThanOrEqualTo: DateTime.now().toString())
+        .get()
+        .then((value) {
       value.docs.forEach((result) {
         final value = EventModel.fromJson(result.data());
+
         events.add(value);
+        //print(value.startTime);
       });
     });
-    print(events[1].city);
+
     return events;
+  }
+
+  // getMonthYear(date) {
+  //   var subsY = date.substring(0, 4);
+  //   var subsM = date.substring(5, 7);
+  //   String conct = subsM + "." + subsY;
+  //   return conct;
+  // }
+
+  // getMonthYearNow(date) {
+  //   var formatter = new DateFormat('MM-yyyy-dd');
+  //   String monthYear = formatter.format(date);
+  //   return monthYear;
+  // }
+
+  monthNext() {
+    var now = new DateTime.now();
+    var datePost = new DateTime(now.year, now.month + 1, now.day);
+    String date = datePost.toString();
+    return date;
   }
 }
