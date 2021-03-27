b7eef0dd92e0855264eb5c6dd03b9ecf6d42801a
Daniela Zavala <danny15zavalalic@gmail.com>
2021-03-27 01:28:07 -0600

    Mostrando categorias y detalles de evento

--- lib/pages/event_page.dart
+++ lib/pages/event_page.dart
@@ -38,52 +38,183 @@ class EventPage extends StatelessWidget {
     );
   }
 
-  Widget _bodyCreate(data) {
+  Widget _bodyCreate(EventModel data) {
     return Container(
       child: Column(
+        children: [_dataEvent(data), _showCategory(data.categories)],
+      ),
+    );
+  }
+
+  Widget _showCategory(List categories) {
+    return FutureBuilder(
+      //future: _eventprovider.getEvents(),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if (categories.length > 0) {
+          final data = categories;
+          if (data.length > 0) {
+            return Expanded(
+              child: ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, i) =>
+                      _createListEvent(data[i], context)),
+            );
+          } else {
+            return Center(
+              child: Text("No se encontraron Categorias"),
+            );
+          }
+        } else {
+          print('jmmm');
+          return Container(
+            margin: EdgeInsets.only(top: 300),
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _createListEvent(Map cat, BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 10, right: 10, left: 10),
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Color.fromRGBO(253, 253, 253, 1),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 1,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Column(
         children: [
-          _dataEvent(data),
-          //  _showCategory()
+          Row(
+            children: [
+              Container(
+                  margin: EdgeInsets.only(top: 12),
+                  child: Image(
+                    image: AssetImage("assets/lugares.jpg"),
+                    width: 45,
+                  )),
+              SizedBox(
+                width: 20,
+              ),
+              Container(
+                width: widthScreen * 0.7,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: [
+                        Text("Categoria: ",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.red[400],
+                                fontSize: 18.0)),
+                        Text(
+                          cat['nameCategory'].toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              //color: Colors.red[400]
+                              fontSize: 23.0),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Premios: ",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red[400]),
+                        ),
+                        Text(cat['prize'].toString())
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Rango de Edad admitido: ",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red[400]),
+                        ),
+                        Text(cat['ageMin'] + " - " + cat['ageMax'] + " años")
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "kilómetros a recorrer: ",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red[400]),
+                        ),
+                        Text(cat['km'].toString())
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "Ruta a Seguir: ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.red[400]),
+                    ),
+                    SizedBox(
+                        height: 50,
+                        child: Center(
+                          child: Text('Aquí va el mapa'),
+                        ))
+                  ],
+                ),
+              )
+            ],
+          ),
+          // Container(
+          //   margin: EdgeInsets.only(left: widthScreen * 0.65),
+          //   child: Row(
+          //     children: [
+          //       Text(
+          //         "Ver mas",
+          //         style: TextStyle(
+          //             fontWeight: FontWeight.bold, color: Colors.red[400]),
+          //       ),
+          //       IconButton(
+          //           icon: Icon(Icons.arrow_forward_ios_outlined,
+          //               color: Colors.red[400]),
+          //           onPressed: () =>
+          //               Navigator.pushNamed(context, 'event', arguments: data)),
+          //     ],
+          //   ),
+          // )
         ],
       ),
     );
   }
-// Widget _showCategory() {
-//     return FutureBuilder(
-//       //future: _eventprovider.getEvents(),
-//       builder:
-//           (BuildContext context, AsyncSnapshot<List<EventModel>> snapshot) {
-//         if (snapshot.hasData) {
-//           final data = snapshot.data;
-//           if (data.length > 0) {
-//             return Expanded(
-//               child: ListView.builder(
-//                   itemCount: data.length,
-//                   itemBuilder: (context, i) =>
-//                       _createListEvent(data[i], context)),
-//             );
-//           } else {
-//             return Center(
-//               child: Text("No se encontraron eventos"),
-//             );
-//           }
-//         } else {
-//           return Container(
-//             margin: EdgeInsets.only(top: 300),
-//             child: Center(
-//               child: CircularProgressIndicator(),
-//             ),
-//           );
-//         }
-//       },
-//     );
-//   }
 
-  Widget _createListEvent() {}
-
-  Widget _dataEvent(data) {
+  Widget _dataEvent(EventModel data) {
     return Container(
-      padding: EdgeInsets.only(top: heightScreen * 0.09, bottom: 10),
+      padding: EdgeInsets.only(top: heightScreen * 0.04, bottom: 10),
       decoration: BoxDecoration(
           border: Border(bottom: BorderSide(color: Colors.red[400])),
           boxShadow: <BoxShadow>[
@@ -95,24 +226,66 @@ class EventPage extends StatelessWidget {
           color: Colors.white),
       child: Column(
         children: [
-          Text("Detalle evento"),
+          Text("Detalle Evento"),
           Divider(),
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
-            child: Row(
+            child: Column(
               children: <Widget>[
                 Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-                  child: Text("HELLO"),
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Se llevará a cabo en: ",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red[400]),
+                        ),
+                        Text(data.city),
+                      ],
+                    )),
+                SizedBox(
+                  height: 5,
                 ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-                  child: Text("HELLO"),
+                Row(
+                  children: [
+                    Text(
+                      "Dará Inicio el: ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.red[400]),
+                    ),
+                    Text(data.startTime.substring(0, 10)),
+                    Text(
+                      " Hora: ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.red[400]),
+                    ),
+                    Text(data.startTime.substring(11, 16)),
+                  ],
                 ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-                  child: Text("HELLO"),
-                )
+                SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Finaliza el: ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.red[400]),
+                    ),
+                    Text(data.endTime.substring(0, 10)),
+                    Text(
+                      " Hora: ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.red[400]),
+                    ),
+                    Text(data.endTime.substring(11, 16)),
+                  ],
+                ),
+                SizedBox(
+                  height: 5,
+                ),
               ],
             ),
           ),
